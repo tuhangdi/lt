feb235f811f919d3f1147aee90d5aacb6b4cebbb
hd <handytu@gmail.com>
2016-08-16 22:28:43 +0800

    1

---
+++ src/ltcode/ListNode.java
@@ -0,0 +1,10 @@
+package ltcode;
+
+/**
+ * Created by hd on 2016/8/16.
+ */
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
--- src/ltcode/Solution.java
+++ src/ltcode/Solution.java
@@ -1,4 +1,4 @@
-/**
+package ltcode; /**
  * Created by hd on 2016/8/16.
  */
 
